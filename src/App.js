1becdf23211e00ff9a552edc35405a2abd8ba9cc
YuraPolischuk <qwerrtuo39@gmail.com>
2023-10-17 11:26:21 +0300

    1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <footer>
+        This project was coded by Yurii Polihchuk and is open-sourced on GitHub
+        and hosted on Netlify
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,83 @@
-function Weather() {
-  return (
-    <div className="Weather">
-      <h1>Kyiv</h1>
-      <form className="d-flex" role="search">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success" type="submit">
-          Search
-        </button>
-      </form>
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+import FormatDate from "./FormatDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
-      <ul>
-        <li>Monday 11:41</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
+function Weather() {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState("Luhansk");
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      date: new Date(response.data.dt * 1000),
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      iconUrl: response.data.weather[0].icon,
+      description: response.data.weather[0].description,
+      city: response.data.name,
+    });
+  }
+
+  function search() {
+    const apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form className="d-flex" role="search" onSubmit={handleSubmit}>
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            autoFocus="on"
+            onChange={handleCityChange}
           />
-          22°C
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humiditi: 72%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>
+            <FormatDate date={weatherData.date} />
+          </li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row indicators">
+          <div className="col-6">
+            <WeatherIcon data={weatherData.iconUrl} />
+            <WeatherTemperature celsius={weatherData.temperature} />
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)} m/s</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
 
 export default Weather;
