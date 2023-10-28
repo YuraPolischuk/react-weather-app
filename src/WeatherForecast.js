3abda0a42d36efc355b0f9992526191857044a26
YuraPolischuk <qwerrtuo39@gmail.com>
2023-10-28 18:01:07 +0300

    fix api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -35,7 +35,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "34ae1065362d42545661451bda2b8a1f";
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
